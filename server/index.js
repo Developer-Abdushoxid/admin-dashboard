54d6633907b2c9795da993ac84661ae30e6f8573
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-06-18 21:03:38 +0500

    install library

---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
+
+
+//  configuration
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
+app.use(morgan('common'))
+
+// routes
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+// mongoose setup
+
+const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, ()=> console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
+
+
+
+
+
+
