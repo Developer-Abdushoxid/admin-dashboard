bf9bac9737d3d0b5f793cc96dbfe48395e4e16f3
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-06-20 09:08:42 +0500

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import { CssBaseline,ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { useSelector } from "react-redux";
-import { themeSettings } from "theme";
 import { useMemo } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+import Layout from 'scenes/layout'
+import Dashboard from "scenes/dashboard";
 
 
 function App() {
@@ -14,6 +16,14 @@ function App() {
       <BrowserRouter>
      <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to='/dashboard' replace />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+
+          </Route>
+        </Route>
+      </Routes>
      </ThemeProvider>
      </BrowserRouter>
     </div>
